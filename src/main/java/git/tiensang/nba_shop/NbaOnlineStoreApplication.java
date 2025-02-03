3b77878c8256403f25098e3f735cc2d2b9578f2a
bindo <bindo2512@gmail.com>
2025-02-03 08:08:13 +0700

    First Commit

---
+++ src/main/java/git/tiensang/nba_shop/NbaOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package git.tiensang.nba_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/tiensang/nba_shop/NbaOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package git.tiensang.nba_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
